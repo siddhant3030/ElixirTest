8f491cc3f7f946dd0f77597e3d436bd9c3926ec9
siddhant3030 <ssiddhant3030@gmail.com>
2019-10-16 21:32:41 +0530

    month sales

--- test5.ex
+++ test5.ex
@@ -3,12 +3,21 @@ records = File.read!("sales.csv")
               |> Enum.map(&String.split(&1,","))
 
 [headers | rows] = records
-# require IEx; IEx.pry
-month_sales = rows
-            |> Enum.map( fn row -> headers |> Enum.zip(row) |> Map.new end)
-            |> Enum.group_by(fn %{"SKU"=> product_name } -> product_name end, fn %{"Quantity"=> quantity } -> String.to_integer(quantity) end )
-            # |> Enum.map(fn %{product_name, quantities} -> Enum.max(quantites) end)
-            |> IO.inspect
+
+items = rows
+        |> Enum.map( fn row -> headers |> Enum.zip(row) |> Map.new end)
+        |> Enum.group_by(fn %{"SKU"=> product_name } -> product_name end, fn %{"Quantity"=> quantity } -> String.to_integer(quantity) end )
+        |> Enum.reduce(%{}, fn ({product_name, quantity}, acc) -> Map.put(acc, product_name, Enum.count(quantity)) end)
+        |> IO.inspect
+
+
+# month = rows
+#         |> Enum.group_by(fn )
+#         |> IO.inspect
+
+
+
+
 
 
 #  [%{ date: '2019-01-01',SKU: 'some detail', unit_price: 5 ,Quantity: 10,total_price:50  }, ]
